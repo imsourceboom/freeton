d3bb7349bc6cbd8e91858af63c9b53fbbf824a8c
imsourceboom <ecmascript.developer@gmail.com>
2020-06-03 18:50:44 +0900

    script error revise

--- src/script/withDraw/index.js
+++ src/script/withDraw/index.js
@@ -2,13 +2,15 @@ const button = document.querySelector("#home > .send");
 const container = document.querySelector("#home > .send-container");
 const close = document.querySelector("#home > .send-container > .close > .box");
 
-console.log(button);
-console.log(container);
-console.log(close);
+// console.log(button);
+// console.log(container);
+// console.log(close);
 
 const onToggle = () => {
   container.classList.toggle("on");
 };
 
-button.addEventListener("click", onToggle);
-close.addEventListener("click", onToggle);
+if (container !== null) {
+  button.addEventListener("click", onToggle);
+  close.addEventListener("click", onToggle);
+}
