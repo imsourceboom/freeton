1460488e6bc6bcd8a0751e0d94dc421bf385ad58
imsourceboom <ecmascript.developer@gmail.com>
2020-06-04 16:46:38 +0900

    create loading page

--- src/script/slideContainer/index.js
+++ src/script/slideContainer/index.js
@@ -1,7 +1,9 @@
 const withdrawBtn = document.querySelector("#home ul.tab > li.withdraw-btn");
 const stakingBtn = document.querySelector("#home ul.tab > li.staking-btn");
+const participationBtn = document.querySelector(".staking-container button");
 const sendContainer = document.querySelector("#home > .send-container");
 const stakingContainer = document.querySelector("#home > .staking-container");
+const investContainer = document.querySelector("#home > .invest-container");
 const back = Array.from(document.querySelectorAll(".slide-header > .back"));
 
 // console.log(withdrawBtn);
@@ -16,15 +18,20 @@ const stakingContainerOnToggle = () => {
   stakingContainer.classList.toggle("on");
 };
 
+const investContainerOnToggle = () => {
+  investContainer.classList.toggle("on");
+};
+
 if (sendContainer !== null) {
   withdrawBtn.addEventListener("click", sendContainerOnToggle);
   back.map((b) => {
     b.addEventListener("click", (e) => {
-      back.map((v) => {
-        if (b === v) {
-          sendContainer.classList.remove("on");
-        }
-      });
+      sendContainer.classList.remove("on");
+      // back.map((v) => {
+      //   if (b === v) {
+      //     sendContainer.classList.remove("on");
+      //   }
+      // });
     });
   });
 }
@@ -33,11 +40,26 @@ if (stakingContainer !== null) {
   stakingBtn.addEventListener("click", stakingContainerOnToggle);
   back.map((b) => {
     b.addEventListener("click", (e) => {
-      back.map((v) => {
-        if (b === v) {
-          stakingContainer.classList.remove("on");
-        }
-      });
+      stakingContainer.classList.remove("on");
+      // back.map((v) => {
+      //   if (b === v) {
+      //     stakingContainer.classList.remove("on");
+      //   }
+      // });
+    });
+  });
+}
+
+if (investContainer !== null) {
+  participationBtn.addEventListener("click", investContainerOnToggle);
+  back.map((b, i) => {
+    b.addEventListener("click", (e) => {
+      investContainer.classList.remove("on");
+      // back.map((v) => {
+      //   if (b === v) {
+      //     investContainer.classList.remove("on");
+      //   }
+      // });
     });
   });
 }
