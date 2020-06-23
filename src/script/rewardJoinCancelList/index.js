cab2007be047dff52ce49db82ff96200a248a12a
imsourceboom <ecmascript.developer@gmail.com>
2020-06-23 16:22:43 +0900

    remove more-btn script and update more-btn style control

--- src/script/rewardJoinCancelList/index.js
+++ src/script/rewardJoinCancelList/index.js
@@ -9,19 +9,13 @@ export function stakingLinkFunc() {
   const joinCancelList = document.querySelector(
     ".reward-set ul.join-cancel-list"
   );
-  const moreBtn = document.querySelector(".reward-set button.more-btn");
 
   if (rewardBtn !== null || joinCancelBtn !== null) {
-    if (moreBtn !== null) {
-      moreBtn.setAttribute("id", "reward_more-btn");
-    }
-
     rewardBtn.addEventListener("click", () => {
       rewardBtn.classList.add("on");
       rewardList.classList.add("on");
       joinCancelBtn.classList.remove("on");
       joinCancelList.classList.remove("on");
-      moreBtn.setAttribute("id", "reward_more-btn");
     });
 
     joinCancelBtn.addEventListener("click", () => {
@@ -29,7 +23,6 @@ export function stakingLinkFunc() {
       rewardList.classList.remove("on");
       joinCancelBtn.classList.add("on");
       joinCancelList.classList.add("on");
-      moreBtn.setAttribute("id", "join_more-btn");
     });
   }
 }
