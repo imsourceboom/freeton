b71e8ddfe1f1dd60c7f9499c378d317979f32174
imsourceboom <ecmascript.developer@gmail.com>
2020-07-30 22:48:17 +0900

    update margin value script

--- src/script/listHeight/index.js
+++ src/script/listHeight/index.js
@@ -10,9 +10,18 @@ export const listHeight = () => {
     const transactionList = document.querySelector(
       "section#wallet > .transaction-list"
     );
+
+    let style = getComputedStyle(topContainer).marginTop;
+    let topContainerMarginHeight = parseInt(
+      style.substring(0, style.length - 2)
+    );
+
     const listHeight =
       window.innerHeight -
-      (header.clientHeight + topContainer.clientHeight + 30 + tab.clientHeight);
+      (header.clientHeight +
+        topContainer.clientHeight +
+        topContainerMarginHeight +
+        tab.clientHeight);
 
     transactionList.style.minHeight = `${listHeight}px`;
   }
