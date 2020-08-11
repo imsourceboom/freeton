74baaae2756f4e68746e06e94f1234a541efd0ba
imsourceboom <ecmascript.developer@gmail.com>
2020-08-11 15:38:38 +0900

    update qrcode script

--- src/script/qrcode/index.js
+++ src/script/qrcode/index.js
@@ -7,28 +7,30 @@ export const qrcode = () => {
     let address = document.getElementById("address");
     address = address.textContent;
 
-    var opts = {
-      errorCorrectionLevel: "L",
-      type: "image/jpeg",
-      scale: 3,
-      quality: 0.5,
-      margin: 1,
-      color: {
-        dark: "#000000ff",
-        light: "#ffffffff",
-      },
-    };
+    if (address !== "") {
+      var opts = {
+        errorCorrectionLevel: "L",
+        type: "image/jpeg",
+        scale: 3,
+        quality: 0.5,
+        margin: 1,
+        color: {
+          dark: "#000000ff",
+          light: "#ffffffff",
+        },
+      };
 
-    QRcode.toDataURL(
-      `https://firston-dev.netlify.app/html/withdraw.html?withdraw-address=${address}`,
-      opts,
-      function (error, url) {
-        // if (error) console.error(error);
-        // console.log("success!");
-        const img = document.getElementById("qrcode");
-        img.src = url;
-      }
-    );
+      QRcode.toDataURL(
+        `https://firston-dev.netlify.app/html/withdraw.html?withdraw-address=${address}`,
+        opts,
+        function (error, url) {
+          // if (error) console.error(error);
+          // console.log("success!");
+          const img = document.getElementById("qrcode");
+          img.src = url;
+        }
+      );
+    }
   }
 };
 
