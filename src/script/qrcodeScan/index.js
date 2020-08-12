5904dfd5ed4957f3b3ae516b4927ac704e99cc35
imsourceboom <ecmascript.developer@gmail.com>
2020-08-12 17:26:30 +0900

    test camera

---
+++ src/script/qrcodeScan/index.js
@@ -0,0 +1,30 @@
+export const qrcodeScan = () => {
+  // if (withDrawSection !== null) {
+  //   let scanner = new Instascan.Scanner({
+  //     video: document.getElementById("qr-scan"),
+  //   });
+  //   controller.addEventListener("click", (e) => {
+  //     scanVideo.classList.toggle("enable");
+  //     if (e.currentTarget.classList.contains("enable")) {
+  //       scanner.stop();
+  //     } else {
+  //       scanner.addListener("scan", function (content) {
+  //         console.log("Wow ", content);
+  //       });
+  //       Instascan.Camera.getCameras()
+  //         .then(function (cameras) {
+  //           if (cameras.length > 0) {
+  //             scanner.start(cameras[0]);
+  //           } else {
+  //             console.error("No cameras found.");
+  //           }
+  //         })
+  //         .catch(function (e) {
+  //           console.error(e);
+  //         });
+  //     }
+  //   });
+  // }
+};
+
+// qrcodeScan();
