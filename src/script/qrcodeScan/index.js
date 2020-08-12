11411ddff522e1e564465589cbb72d05e125d729
imsourceboom <ecmascript.developer@gmail.com>
2020-08-12 21:41:09 +0900

    canvas height

--- src/script/qrcodeScan/index.js
+++ src/script/qrcodeScan/index.js
@@ -9,9 +9,37 @@ export const qrcodeScan = () => {
     const video = document.createElement("video");
     const canvas = canvasElement.getContext("2d");
     const btnScanQR = document.querySelector(".scan-video-controller");
+    const container = document.createElement("div");
+    const comment = document.createElement("h2");
     let outputData;
-
     let scanning = false;
+
+    const scanComplete = () => {
+      container.style.cssText = `
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 999999999999;
+        width: 100%;
+        height: 100vh;
+        background-color: rgba(255, 255, 255, 0.1);
+        backdrop-filter: blur(3px);
+      `;
+
+      comment.style.cssText = `
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate3d(-50%, -50%, 0);
+        color: #34495e;
+        white-space: nowrap;
+        margin-top: -7px;
+      `;
+
+      comment.textContent = "스캔이 완료되었습니다";
+      container.appendChild(comment);
+      document.body.appendChild(container);
+    };
 
     qrcode.callback = (res) => {
       if (res) {
@@ -23,9 +51,12 @@ export const qrcodeScan = () => {
           track.stop();
         });
 
+        scanComplete();
         canvasElement.hidden = true;
 
-        alert(outputData);
+        setTimeout(() => {
+          document.body.removeChild(container);
+        }, 1500);
       }
     };
 
@@ -37,6 +68,7 @@ export const qrcodeScan = () => {
           // qrResult.hidden = true;
           // btnScanQR.hidden = true;
           canvasElement.hidden = false;
+          canvasElement.style.marginBottom = "6px";
           video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
           video.srcObject = stream;
           video.play();
@@ -46,7 +78,7 @@ export const qrcodeScan = () => {
     });
 
     function tick() {
-      canvasElement.height = video.videoHeight;
+      canvasElement.height = video.videoWidth * 0.74;
       canvasElement.width = video.videoWidth;
       canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
 
