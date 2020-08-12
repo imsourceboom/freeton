69024d7d8db6da57b24a45c71f04277360b03aac
imsourceboom <ecmascript.developer@gmail.com>
2020-08-12 21:05:12 +0900

    add qrcode filter

--- src/script/qrcodeScan/index.js
+++ src/script/qrcodeScan/index.js
@@ -3,60 +3,62 @@ import { qrcode } from "./jsqrcode";
 export const qrcodeScan = () => {
   // const jsqrcode = qrcode;
 
-  const video = document.createElement("video");
   const canvasElement = document.getElementById("qr-canvas");
-  const canvas = canvasElement.getContext("2d");
-  const btnScanQR = document.querySelector(".scan-video-controller");
 
-  let outputData;
+  if (canvasElement !== null) {
+    const video = document.createElement("video");
+    const canvas = canvasElement.getContext("2d");
+    const btnScanQR = document.querySelector(".scan-video-controller");
+    let outputData;
 
-  let scanning = false;
+    let scanning = false;
 
-  qrcode.callback = (res) => {
-    if (res) {
-      // outputData.innerText = res;
-      outputData = res;
-      scanning = false;
+    qrcode.callback = (res) => {
+      if (res) {
+        // outputData.innerText = res;
+        outputData = res;
+        scanning = false;
 
-      video.srcObject.getTracks().forEach((track) => {
-        track.stop();
-      });
+        video.srcObject.getTracks().forEach((track) => {
+          track.stop();
+        });
 
-      canvasElement.hidden = true;
+        canvasElement.hidden = true;
 
-      alert(outputData);
+        alert(outputData);
+      }
+    };
+
+    btnScanQR.addEventListener("click", () => {
+      navigator.mediaDevices
+        .getUserMedia({ video: { facingMode: "environment" } })
+        .then(function (stream) {
+          scanning = true;
+          // qrResult.hidden = true;
+          // btnScanQR.hidden = true;
+          canvasElement.hidden = false;
+          video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+          video.srcObject = stream;
+          video.play();
+          tick();
+          scan();
+        });
+    });
+
+    function tick() {
+      canvasElement.height = video.videoHeight;
+      canvasElement.width = video.videoWidth;
+      canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+      scanning && requestAnimationFrame(tick);
     }
-  };
 
-  btnScanQR.addEventListener("click", () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { facingMode: "environment" } })
-      .then(function (stream) {
-        scanning = true;
-        // qrResult.hidden = true;
-        // btnScanQR.hidden = true;
-        canvasElement.hidden = false;
-        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-        video.srcObject = stream;
-        video.play();
-        tick();
-        scan();
-      });
-  });
-
-  function tick() {
-    canvasElement.height = video.videoHeight;
-    canvasElement.width = video.videoWidth;
-    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-
-    scanning && requestAnimationFrame(tick);
-  }
-
-  function scan() {
-    try {
-      qrcode.decode();
-    } catch (e) {
-      setTimeout(scan, 300);
+    function scan() {
+      try {
+        qrcode.decode();
+      } catch (e) {
+        setTimeout(scan, 300);
+      }
     }
   }
 };
